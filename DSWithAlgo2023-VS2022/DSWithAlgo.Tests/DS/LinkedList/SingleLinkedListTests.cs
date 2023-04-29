4a7c833420785ade3f4ef29e54086c4e2bcae600
Sangkon, Han <sigmadream@gmail.com>
2023-04-29 23:28:44 +0900

    단일 연결 리스트(Single Linked List) 구현 (#3)
    feat: SingleLinkedList AddLast, Length 서드 추가
    feat: 나머지 메서드 및 타입을 <T>로 변경

---
+++ DSWithAlgo2023-VS2022/DSWithAlgo.Tests/DS/LinkedList/SingleLinkedListTests.cs
@@ -0,0 +1,64 @@
+using Xunit;
+using DSWithAlgo.DS.LinkedList;
+
+namespace DSWithAlgo.Tests.LinkedList
+{
+    public static class LinkedListTests
+    {
+        [Fact]
+        public static void AddLast()
+        {
+            var ll = new SingleLinkedList<string>();
+            ll.AddLast("X");
+            ll.AddLast("Y");
+            ll.AddLast("Z");
+            Assert.Equal(3, ll.Length());
+
+            var ll2 = new SingleLinkedList<int>();
+            ll2.AddLast(1);
+            ll2.AddLast(2);
+            ll2.AddLast(3);
+            ll2.AddLast(4);
+            Assert.Equal(4, ll2.Length());
+        }
+
+        [Fact]
+        public static void AddFirst()
+        {
+            var ll = new SingleLinkedList<int>();
+            ll.AddFirst(1);
+            ll.AddFirst(15);
+            Assert.Equal(2, ll.Length());
+
+            var ll2 = new SingleLinkedList<string>();
+            ll2.AddLast("1");
+            ll2.AddLast("2");
+            ll2.AddLast("3");
+            ll2.AddLast("4");
+            Assert.Equal(4, ll2.Length());
+        }
+
+        [Fact]
+        public static void GetData()
+        {
+            var ll = new SingleLinkedList<string>();
+            ll.AddLast("X");
+            ll.AddLast("Y");
+            ll.AddLast("Z");
+
+            Assert.Equal("Y", ll.Index(1));
+        }
+
+        [Fact]
+        public static void Delete()
+        {
+            var ll = new SingleLinkedList<int>();
+            ll.AddFirst(1);
+            ll.AddFirst(2);
+            ll.AddFirst(3);
+
+            var result = ll.Delete(2);
+            Assert.True(result);
+        }
+    }
+}
---
+++ DSWithAlgo2023-VSC/TODO.md
@@ -0,0 +1,18 @@
+# Linked List
+
+## ADT
+
+- Add(Last, First)
+- Remove
+- Read
+- Index
+
+## Imp.
+
+- [x] Node
+- [x] LinkedList.AddLast()
+- [x] LinkedList.AddFirst()
+- [x] LinkedList.Delete()
+- [x] LinkedList.Length()
+- [x] LinkedList.GetData()
+- [x] LinkedList.Index()
