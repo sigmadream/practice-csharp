dc905fdb2c82b71a27ec0d3b4537c78b2bab0230
Sangkon Han <sigmadream@gmail.com>
2023-08-07 19:45:32 +0900

    feat: LinkedList 기반의 Queue 추가
    - Generic에 포함된 LinkedList를 활용

---
+++ DSWithAlgo/DS/Queue/LinkedListQueue.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSWithAlgo.DS.Queue
+{
+    public class LinkedListQueue<T>
+    {
+        private readonly LinkedList<T> queue;
+        public LinkedListQueue() => queue = new LinkedList<T>();
+
+        public void Enqueue(T item)
+        {
+            queue.AddLast(item);
+        }
+
+        public T Dequeue()
+        {
+            if (queue.First == null)
+            {
+                throw new InvalidOperationException("There are no item in the queue.");
+            }
+
+            var item = queue.First;
+            queue.RemoveFirst();
+            return item.Value;
+        }
+
+        public T Peek()
+        {
+            if (queue.First is null)
+            {
+                throw new InvalidOperationException("There are no items in the queue.");
+            }
+
+            return queue.First.Value;
+        }
+
+        public bool IsEmpty() => !queue.Any();
+    }
+}
