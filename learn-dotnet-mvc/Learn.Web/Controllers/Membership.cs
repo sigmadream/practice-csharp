78811aedad7564d50f2968d58e99d9c721e3f24c
Sangkon Han <sigmadream@gmail.com>
2023-12-19 03:22:57 +0900

    feat: ASP.NET 튜토리얼 #1

---
+++ learn-dotnet-mvc/Learn.Web/Controllers/Membership.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Learn.Web.Models;
+
+namespace Learn.Web.Controllers
+{
+    public class Membership : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Login(LoginInfo login)
+        {
+            string message = string.Empty;
+
+            if (ModelState.IsValid)
+            {
+                string userId = "admin";
+                string password = "qwer1234";
+
+                if (login.UserId == userId && login.Password == password)
+                {
+                    TempData["Message"] = "로그인에 성공했습니다.";
+                    return RedirectToAction("Index", "Membership");
+                }
+                else
+                {
+                    message = "ID와 비밀번호를 확인해주세요.";
+                }
+            }
+            else
+            {
+                message = "로그인 정보가 올바르지 않습니다.";
+            }
+            ModelState.AddModelError(string.Empty, message);
+            return View();
+        }
+    }
+}
---
+++ learn-dotnet-mvc/Learn.Web/Models/LoginInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Learn.Web.Models
+{
+    public class LoginInfo
+    {
+        [Required(ErrorMessage = "사용자 아이디를 입력하세요.")]
+        [MinLength(4, ErrorMessage = "사용자 아이디는 4자 이상 입력하세요.")]
+        [Display(Name = "사용자 아이디")]
+        public string UserId { get; set; }
+
+        [Required(ErrorMessage = "비밀번호를 입력하세요.")]
+        [MinLength(4, ErrorMessage = "비밀번호는 4자 이상 입력하세요.")]
+        [Display(Name = "비밀번호")]
+        public string Password { get; set; }
+    }
+}
